83d223219b767c8a9f220d0652b0f57603a61ba4
Julio Eduardo Canizalez Salinas <juliocanizalez@outlook.com>
2021-03-04 01:00:48 -0600

    :sparkles: finishes ShuffleString problem

--- ShuffleString/Program.cs
+++ ShuffleString/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace ShuffleString
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShuffleString
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
             
+        }
+        
+        public static string RestoreString(string s, int[] indices)
+        {
+            string aux = "";
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            for (int i = 0; i < indices.Length; i++)
+            {
+                dic.Add(indices[i], s[i].ToString());
+            }
+
+            for (int i = 0; i < indices.Length; i++)
+            {
+                aux += dic[i];
+            }
+            return aux;
         }
     }
 }
